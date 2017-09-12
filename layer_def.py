8cd92983d92b30763879aa5b98ab433a79ce1b1c
YukiOta <yuki.ota@ruri.waseda.jp>
2017-09-12 18:44:12 +0900

    from AIST 0912

---
+++ layer_def.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+"""CONV_LSTMにおけるレイヤーの定義
+
+"""
+
+import tensorflow as tf
+import numpy as np
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_float('weight_decay', 0.0005, """ """)
+
+a = np.zeros((100, 100, 3))
+matrix1 = tf.constant(a)
+matrix1.get_shape()[2]
+
+def conv/layer(inputs, kernel_size, stride, num_features, idx, linear=False):
+    with tf.variable_scope('{0}_conv'.format(idx)) as scope:
+        input_channels = inputs.get_shape
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end
